3521b394191057b61f14a4a355a556afb64bd46a
Liu Link <ctliu5link@hotmail.com>
2024-07-23 09:29:31 +0800

    升級到.NET 8 並移除舊版本

--- SchemaNote_6.0/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace SchemaNote.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- SchemaNote_6.0/sql/getExtended_prop.sql
+++
@@ -1,9 +0,0 @@
-SELECT [major_id]  AS [MAJOR_ID]
-       ,[minor_id] AS [MINOR_ID]
-       ,[name]     AS [NAME]
-       ,[value]    AS [VALUE]
-       ,[class]    AS [CLASS]
-  FROM sys.extended_properties
- WHERE NOT ([minor_id] = 0
-        AND [class] = 1
-        AND [name] = N'microsoft_database_tools_support') 
