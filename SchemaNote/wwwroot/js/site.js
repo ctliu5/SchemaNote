ec2a9070a9e39775043f10a3b65348ee032cf141
Link Liu <ctliu5@hotmail.com>
2026-09-15 20:48:13 +0800

    改用 Blob 與 createObjectURL 產生下載連結
    將 download 函式由 data URI 改為使用 Blob 物件與 window.URL.createObjectURL，避免瀏覽器在 HTTP 連線下出現安全警告，提升檔案下載的相容性與穩定性。

--- SchemaNote/wwwroot/js/site.js
+++ SchemaNote/wwwroot/js/site.js
@@ -286,9 +286,9 @@ function download(filename, text) {
 
     text = '\ufeff' + text; //for windows OS, convert『UTF-8』 to 『UTF-8 with bom』,see https://stackoverflow.com/questions/17879198/adding-utf-8-bom-to-string-blob
 
-    // 使用 data: URI 取代 blob: URL，避免瀏覽器在 HTTP 連線下發出
-    // 「loaded over an insecure connection」的安全警告。
-    var url = 'data:text/plain;charset=UTF-8,' + encodeURIComponent(text);
+    // var url = 'data:text/plain;charset=UTF-8,' + encodeURIComponent(text);
+    var blob = new Blob([text], { type: 'text/plain;charset=UTF-8' });
+    var url = window.URL.createObjectURL(blob);
 
     var element = document.createElement('a');
 
